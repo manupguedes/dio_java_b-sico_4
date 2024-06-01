811c67f9eec9c41a6832145a87829d67f59a131b
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 16:56:06 -0300

    Cliente.java

--- DioJavaBasico4/src/BancoDigital2/Cliente.java
+++ DioJavaBasico4/src/BancoDigital2/Cliente.java
@@ -1,5 +1,12 @@
 package BancoDigital2;
-
 public class Cliente {
+    private String nome;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
