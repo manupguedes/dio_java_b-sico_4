094a8784e54820325f11d2297179ed078ba8b159
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 16:56:46 -0300

    Conta.java

--- DioJavaBasico4/src/BancoDigital2/Conta.java
+++ DioJavaBasico4/src/BancoDigital2/Conta.java
@@ -1,5 +1,48 @@
 package BancoDigital2;
+public abstract class Conta {
+    private static int SEQUENCIAL = 1;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
 
-public class Conta {
+    public Conta() {
+        this.agencia = 1;
+        this.numero = SEQUENCIAL++;
+        this.saldo = 0.0;
+    }
 
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        }
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        if (valor <= saldo) {
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+        }
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void imprimirExtrato() {
+        System.out.println("Agencia: " + agencia);
+        System.out.println("Numero: " + numero);
+        System.out.println("Saldo: " + saldo);
+    }
 }
