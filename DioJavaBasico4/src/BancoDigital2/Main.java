b50fa4f5d6bcb50edb6320ef1c26ec2439bfd0c1
Manoela <148467291+manupguedes@users.noreply.github.com>
2024-06-01 16:58:24 -0300

    Main.java

--- DioJavaBasico4/src/BancoDigital2/Main.java
+++ DioJavaBasico4/src/BancoDigital2/Main.java
@@ -1,5 +1,16 @@
 package BancoDigital2;
-
 public class Main {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente();
+        cliente1.setNome("João");
 
+        Conta cc = new ContaCorrente(cliente1);
+        Conta poupanca = new ContaPoupança(cliente1);
+
+        cc.depositar(100);
+        cc.transferir(50, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
 }
